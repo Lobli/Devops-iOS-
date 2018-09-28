a0b372d6fa2c9ed68135023caeb44c38727325aa
Adam Lobler <lobleradam@mllnC3E442C6.fixip.t-online.hu>
2018-09-28 13:30:44 +0200

    Update travis config

---
+++ MusicPlayer_iOSTests/MusicPlayer_iOSTests.swift
@@ -0,0 +1,33 @@
+//
+//  MusicPlayer_iOSTests.swift
+//  MusicPlayer_iOSTests
+//
+//  Created by Lobler Ádám on 2018. 09. 28..
+//  Copyright © 2018. Lobler Ádám. All rights reserved.
+//
+
+import XCTest
+
+class MusicPlayer_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
